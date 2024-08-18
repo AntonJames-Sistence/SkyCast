47621488a8dab96916949ad5b2394dbd2fb40442
AntonJames-Sistence <anton.james.ja@gmail.com>
2024-08-18 12:54:59 -0400

    Add MVP for city weather

--- src/WeatherWidget/WeatherWidget.js
+++ src/WeatherWidget/WeatherWidget.js
@@ -3,17 +3,65 @@ import React, { useEffect, useState } from "react";
 const WEATHER_API_KEY = "2152de8ca0fcc349444eabd7c3670f68";
 
 const WeatherWidget = ({ city }) => {
-  const [weather, setWeather] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    fetchWeatherData();
+  }, [city]);
 
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_API_KEY}`
+      );
+      if (!res.ok) {
+        throw new Error(`City not found: ${city}`);
+      }
+      const data = await res.json();
+      console.log(data); // remove before prod
+      setWeatherData(data);
+      setError(null);
+    } catch (error) {
+      // Set the error message
+      setError(error.message);
+      setWeatherData(null);
+    }
+  };
+
+  if (error) {
+    return (
+      <div className="text-red-500" aria-live="assertive">
+        {error}
+      </div>
+    );
+  }
 
   // Function to convert temperature from Kelvin to Fahrenheit
   const KtoF = (tempKevlin) => {
     return ((tempKevlin - 273.15) * 9) / 5 + 32;
+  };
+
+  if (!weatherData) {
+    return <div aria-live="polite">loading...</div>;
   }
 
-  return <div>{`Hello app`}</div>;
+  const iconUrl = `https://openweathermap.org/img/wn/${weatherData?.weather[0].icon}@2x.png`;
+  return (
+    <div className="shadow-lg text-gray-500 p-4 flex flex-col justify-center text-center bg-white rounded-lg">
+      <h2 className="uppercase font-bold text-2xl ">{city}</h2>
+      <img src={iconUrl} className="self-center" alt="weather icon" />
+      <div className="capitalize mb-4">
+        {weatherData?.weather[0].description}
+      </div>
+      <div>
+        <span className="mr-4">Temperature:</span>
+        <span className="font-semibold text-black text-3xl">
+          {KtoF(weatherData?.main.temp).toFixed(0)} &#8457;
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherWidget;
